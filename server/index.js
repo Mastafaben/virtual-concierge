11a95c373bed0fbff7a8fba53ab9b6c6a11f0c7b
MUSTAPHA BENDAOUD <123925289+Mastafaben@users.noreply.github.com>
2026-03-17 14:19:40 +0000

    Initial commit: full Virtual Concierge SaaS

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const { Server } = require('socket.io');
+const http = require('http');
+
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Virtual Concierge Backend');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
